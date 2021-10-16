5d5aef99eb2f10488f8d276ed20c7aa3b7591cca
ezko_ <electronics@elec.mn>
2021-10-16 16:33:13 +0800

    language changed

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get("/home",[HomeController::class, "index"]);
